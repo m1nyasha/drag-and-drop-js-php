21517f4af1f9bde61075ec87d3cc9b1f21652e08
Mikhail Protasevich <m.protasevich@smartcat.ai>
2021-05-03 13:28:31 +0300

    initial commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Drag & Drop</title>
+    <link rel="stylesheet" href="src/scss/app.css">
+</head>
+<body>
+<div class="drag-and-drop">
+    <h3 class="drag-and-drop-title">
+        Перетащите файл для загрузки
+    </h3>
+</div>
+<div class="images-list">
+</div>
+<button class="upload-btn" disabled="true" onclick="uploadImages()">Upload images</button>
+<script src="src/js/app.js"></script>
+</body>
+</html>
---
+++ src/js/app.js
@@ -0,0 +1,60 @@
+// dragenter - когда сущность над элементом
+// dragleave - когда сущность вышла за пределы элемента
+// dragover - срабатывает постоянно, когда сущность над элементом
+// drop - срабатывает, когда объект был отпущен над элементом
+
+let imagesForUpload = [];
+
+const types = ['image/jpeg', 'image/png']
+
+let dragAndDrop = document.querySelector('.drag-and-drop'),
+    imagesList = document.querySelector('.images-list'),
+    uploadBtn = document.querySelector('.upload-btn')
+
+dragAndDrop.addEventListener('dragenter', (e) => {
+    e.preventDefault()
+    dragAndDrop.classList.add('active')
+})
+dragAndDrop.addEventListener('dragleave', (e) => {
+    e.preventDefault()
+    dragAndDrop.classList.remove('active')
+})
+dragAndDrop.addEventListener('dragover', (e) => {
+    e.preventDefault()
+})
+
+dragAndDrop.addEventListener('drop', (e) => {
+    e.preventDefault()
+    const files = e.dataTransfer.files
+    for (let key in files) {
+        if (!types.includes(files[key].type)) {
+            continue;
+        }
+        imagesForUpload.push(files[key])
+        let imageTmpUrl = URL.createObjectURL(files[key])
+        imagesList.innerHTML += `<img src="${imageTmpUrl}" class="images-list-picture" alt="">`
+    }
+    if (imagesForUpload.length > 0) {
+        uploadBtn.removeAttribute('disabled')
+    }
+})
+
+const uploadImages = () => {
+    let formData = new FormData()
+    for (let key in imagesForUpload) {
+        formData.append(key, imagesForUpload[key])
+    }
+    fetch('/core/upload.php', {
+        method: "POST",
+        body: formData
+    })
+        .then(response => response.json())
+        .then(result => {
+            if (result.status) {
+                imagesForUpload = []
+                imagesList.innerHTML = ``
+                uploadBtn.setAttribute('disabled', true)
+                alert('Файлы успешно загружены')
+            }
+        })
+}
